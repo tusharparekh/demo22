5d2d7aae43fc925584cdba1728f24d6ddc8ae7d2
TUSHAR PAREKH <tusharparekh1@gmail.com>
2020-08-21 10:49:31 +0530

    Initial Commit by Tushar

---
+++ src/test/java/com/example/demo22/Demo22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
